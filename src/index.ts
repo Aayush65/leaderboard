76cdc8e4adc7af16feecff895a366c2a4eb4105e
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-06-19 20:06:55 +0530

    added .env file and secured the pass

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express, {Request, Response} from 'express';
+import { config } from 'dotenv';
 import mongoose from 'mongoose';
 import LeaderboardModel from './models/score';
 
-
+config();
 const app = express();
 app.use(express.json());
-
-// console.log(process.env.URL)
 
 
 app.get('/', (req: Request, res: Response) => {
@@ -20,10 +19,10 @@ app.post('/entry', async (req: Request, res: Response) => {
     res.json(addScore);
 })
 
-mongoose.connect("mongodb+srv://mantis65:UMKzZm2cux2q9Eaf@leaderboardcluster.2halqrp.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(process.env.URL!)
         .then(() => {
-            console.log("Connected to MongoDB and Listening on Port 5000");
-            app.listen(5000);
+            console.log(`Connected to MongoDB and Listening on Port ${process.env.PORT || 5000}`);
+            app.listen(process.env.PORT || 5000);
         })
         .catch((err) => {
             console.log("Can't connect to the db");
