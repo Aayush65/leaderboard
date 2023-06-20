2bc9ac6145fc266c13647e1f159636335d31e0e0
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-06-20 13:25:16 +0530

    Made endpoint /entry for get and post

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,39 @@
 import express, {Request, Response} from 'express';
 import { config } from 'dotenv';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import LeaderboardModel from './models/score';
 
 config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).send("Hello World");
-})
 
 app.post('/entry', async (req: Request, res: Response) => {
     const { name, clicks } = req.body;
-    const newScore = new LeaderboardModel({ name, clicks });
-    const addScore = await newScore.save();
-    res.json(addScore);
+    if (!name){
+        res.status(400).json({ message: "Missing name" });
+    } else if (!clicks) {
+        res.status(400).json({ message: "Missing clicks" });
+    } else {
+        const newScore = new LeaderboardModel({ name, clicks });
+        const addScore = await newScore.save();
+        res.json(addScore);
+    }
 })
 
-mongoose.connect(process.env.URL!)
+
+app.get('/entry', async (req: Request, res: Response) => {
+    const scores = await LeaderboardModel.find().sort({ clicks: 1}).limit(5);
+    res.json(scores);
+})
+
+mongoose.connect(process.env.MONGO_URL!)
         .then(() => {
-            console.log(`Connected to MongoDB and Listening on Port ${process.env.PORT || 5000}`);
-            app.listen(process.env.PORT || 5000);
+            console.log(`Connected to MongoDB and Listening on Port ${process.env.PORT}`);
+            app.listen(process.env.PORT);
         })
         .catch((err) => {
             console.log("Can't connect to the db");
